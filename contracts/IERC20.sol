f60f857b89e0e190e5dcbfe0c950ad09c0c0e1c7
ruiqingluo <ruiqingluo1@163.com>
2021-07-30 16:42:02 +0800

    update contracts

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -6,60 +6,34 @@ pragma solidity ^0.4.24;
  */
 interface IERC20 {
 
-  /**
-  * @dev Total number of tokens in existence
-  */
-  function totalSupply() external view returns (uint256);
+  /// @param _owner The address from which the balance will be retrieved
+  /// @return balance the balance
+  function balanceOf(address _owner) external view returns (uint256 balance);
 
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param owner The address to query the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address owner) external view returns (uint256);
+  /// @notice send `_value` token to `_to` from `msg.sender`
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return success Whether the transfer was successful or not
+  function transfer(address _to, uint256 _value)  external returns (bool success);
 
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param owner address The address which owns the funds.
-   * @param spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
-  function allowance(address owner, address spender)
-    external view returns (uint256);
+  /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+  /// @param _from The address of the sender
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return success Whether the transfer was successful or not
+  function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
 
-  /**
-  * @dev Transfer token for a specified address
-  * @param to The address to transfer to.
-  * @param value The amount to be transferred.
-  */
-  function transfer(address to, uint256 value) external returns (bool);
+  /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @param _value The amount of wei to be approved for transfer
+  /// @return success Whether the approval was successful or not
+  function approve(address _spender  , uint256 _value) external returns (bool success);
 
-  /**
-   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   * @param spender The address which will spend the funds.
-   * @param value The amount of tokens to be spent.
-   */
-  function approve(address spender, uint256 value)
-    external returns (bool);
+  /// @param _owner The address of the account owning tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @return remaining Amount of remaining tokens allowed to spent
+  function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
-  /**
-   * @dev Transfer tokens from one address to another
-   * @param from address The address which you want to send tokens from
-   * @param to address The address which you want to transfer to
-   * @param value uint256 the amount of tokens to be transferred
-   */
-  function transferFrom(address from, address to, uint256 value)
-    external returns (bool);
-
-  event Transfer(
-    address indexed from,
-    address indexed to,
-    uint256 value
-  );
-
-  event Approval(
-    address indexed owner,
-    address indexed spender,
-    uint256 value
-  );
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
